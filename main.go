e7e863c6cce05e10461fbecde514eb4824d25de0
lemonc <lvmengcheng527@163.com>
2026-02-10 17:32:46 +0800

    重构代码，配置文件里添加设置项

--- main.go
+++ main.go
@@ -20,51 +20,86 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const (
-	DefaultUDPTimeout = 120 * time.Second // 会话超时时间
-	CleanupInterval   = 30 * time.Second  // 清理间隔
-	BufferSize        = 65535             // 64KB
-)
+// 配置
+type Config struct {
+	Setting Setting       `yaml:"setting"`
+	Tunnels []TunnelGroup `yaml:"tunnels"`
+}
 
-// TunnelItem 单个隧道配置
-type TunnelItem struct {
+type Setting struct {
+	SessionTimeout   int `yaml:"session_timeout"`
+	CleanupInterval  int `yaml:"cleanup_interval"`
+	SocketBufSize    int `yaml:"socket_buf_size"`
+	PacketBufferSize int `yaml:"packet_buffer_size"`
+}
+
+type TunnelGroup struct {
+	Host     string        `yaml:"host"`
+	Services []ServiceItem `yaml:"services"`
+}
+
+type ServiceItem struct {
 	Name   string `yaml:"name"`
 	Remote int    `yaml:"remote"`
 	Local  int    `yaml:"local"`
 	Proto  string `yaml:"proto"`
 }
 
-// Config 配置文件映射
-type Config map[string][]TunnelItem
-
-// --- 全局资源池 ---
-var bufferPool = sync.Pool{
-	New: func() any {
-		return make([]byte, BufferSize)
-	},
+// --- 核心管理器 ---
+type Manager struct {
+	ctx             context.Context
+	wg              sync.WaitGroup
+	sessionTimeout  time.Duration
+	cleanupInterval time.Duration
+	socketBufSize   int
+	bufferPool      sync.Pool
 }
 
-func putBack(b []byte) {
-	if b != nil {
-		bufferPool.Put(b[:cap(b)])
+func NewManager(ctx context.Context, cfg Config) *Manager {
+	s := cfg.Setting
+	// 默认设置
+	if s.SessionTimeout <= 0 {
+		s.SessionTimeout = 120
+	}
+	if s.CleanupInterval <= 0 {
+		s.CleanupInterval = 30
+	}
+	if s.SocketBufSize <= 0 {
+		s.SocketBufSize = 4
+	}
+	if s.PacketBufferSize <= 0 || s.PacketBufferSize > 64 {
+		s.PacketBufferSize = 64
+	}
+
+	return &Manager{
+		ctx:             ctx,
+		sessionTimeout:  time.Duration(s.SessionTimeout) * time.Second,
+		cleanupInterval: time.Duration(s.CleanupInterval) * time.Second,
+		socketBufSize:   s.SocketBufSize * 1024 * 1024,
+		bufferPool: sync.Pool{
+			New: func() any {
+				return make([]byte, s.PacketBufferSize*1024)
+			},
+		},
 	}
 }
 
-// --- 核心管理器 ---
-type Manager struct {
-	ctx context.Context
-	wg  sync.WaitGroup
+func (m *Manager) getBuf() []byte {
+	return m.bufferPool.Get().([]byte)
 }
 
-func NewManager(ctx context.Context) *Manager {
-	return &Manager{ctx: ctx}
+func (m *Manager) putBuf(b []byte) {
+	if b != nil {
+		m.bufferPool.Put(b[:cap(b)])
+	}
 }
 
 func (m *Manager) Start(cfg Config) {
-	for host, tunnels := range cfg {
-		for _, t := range tunnels {
+	for _, tunnel := range cfg.Tunnels {
+		for _, t := range tunnel.Services {
 			if t.Local <= 0 || t.Local > 65535 || t.Remote <= 0 || t.Remote > 65535 {
-				log.Printf("[WARN] (%s) 跳过无效端口配置: %s, Local: %d, Remote: %d", t.Name, host, t.Local, t.Remote)
+				log.Printf("[WARN] (%s) 跳过无效端口配置: %s, Local: %d, Remote: %d",
+					t.Name, tunnel.Host, t.Local, t.Remote)
 				continue
 			}
 
@@ -79,7 +114,7 @@ func (m *Manager) Start(cfg Config) {
 			}
 
 			local := fmt.Sprintf(":%d", t.Local)
-			remote := net.JoinHostPort(host, strconv.Itoa(t.Remote))
+			remote := net.JoinHostPort(tunnel.Host, strconv.Itoa(t.Remote))
 
 			// 启动对应的桥接服务
 			if proto == "tcp" {
@@ -161,30 +196,41 @@ func (m *Manager) handleTCPConn(clientConn *net.TCPConn, remoteAddr, name string
 	defer remoteConn.Close()
 	_ = remoteConn.SetNoDelay(true)
 
-	// 监听关闭信号
-	go func() {
-		<-m.ctx.Done()
+	// 如果全局ctx取消，关闭这两个socket
+	stop := context.AfterFunc(m.ctx, func() {
 		_ = clientConn.Close()
 		_ = remoteConn.Close()
-	}()
+	})
+	defer stop()
 
-	// 内部双向读写 WaitGroup
-	var internalWg sync.WaitGroup
-	transfer := func(dst, src *net.TCPConn, dir string) {
-		buf := bufferPool.Get().([]byte)
-		defer putBack(buf)
-		if _, err := io.CopyBuffer(dst, src, buf); err != nil {
+	// 双向读写
+	done := make(chan struct{}, 1)
+	go func() {
+		buf := m.getBuf()
+		defer m.putBuf(buf)
+		if _, err := io.CopyBuffer(remoteConn, clientConn, buf); err != nil {
 			if !m.isClosed(err) {
-				log.Printf("[WARN] (%s) %s 数据转发异常: %v", name, dir, err)
+				log.Printf("[WARN] (%s) 客户端->服务端 数据转发异常: %v", name, err)
 			}
 		}
 		// 半关闭，告诉对方已经写完了
-		_ = dst.CloseWrite()
-	}
+		_ = remoteConn.CloseWrite()
 
-	internalWg.Go(func() { transfer(remoteConn, clientConn, "客户端->服务端") })
-	internalWg.Go(func() { transfer(clientConn, remoteConn, "服务端->客户端") })
-	internalWg.Wait()
+		done <- struct{}{}
+	}()
+
+	buf := m.getBuf()
+	defer m.putBuf(buf)
+	if _, err := io.CopyBuffer(clientConn, remoteConn, buf); err != nil {
+		if !m.isClosed(err) {
+			log.Printf("[WARN] (%s) 服务端->客户端 数据转发异常: %v", name, err)
+		}
+	}
+	// 半关闭，告诉对方已经写完了
+	_ = remoteConn.CloseWrite()
+
+	// 阻塞等两个进程运行结束
+	<-done
 }
 
 type udpSession struct {
@@ -211,13 +257,13 @@ func (m *Manager) runUDP(local, remote, name string) {
 	}
 	defer conn.Close()
 
-	go func() {
-		<-m.ctx.Done()
+	stop := context.AfterFunc(m.ctx, func() {
 		_ = conn.Close()
-	}()
+	})
+	defer stop()
 
-	_ = conn.SetReadBuffer(2 * 1024 * 1024)
-	_ = conn.SetWriteBuffer(2 * 1024 * 1024)
+	_ = conn.SetReadBuffer(m.socketBufSize)
+	_ = conn.SetWriteBuffer(m.socketBufSize)
 
 	var (
 		sessions = make(map[netip.AddrPort]*udpSession)
@@ -226,39 +272,17 @@ func (m *Manager) runUDP(local, remote, name string) {
 
 	// 定期清理协程
 	m.wg.Go(func() {
-		ticker := time.NewTicker(CleanupInterval)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-m.ctx.Done():
-				mu.Lock()
-				for _, s := range sessions {
-					_ = s.conn.Close()
-				}
-				mu.Unlock()
-				return
-			case <-ticker.C:
-				now := time.Now().Unix()
-				mu.Lock()
-				for k, s := range sessions {
-					if now-s.lastActive.Load() > int64(DefaultUDPTimeout.Seconds()) {
-						_ = s.conn.Close()
-						delete(sessions, k)
-					}
-				}
-				mu.Unlock()
-			}
-		}
+		m.sessionsCleanup(sessions, &mu)
 	})
 
 	log.Printf("[INFO] (%s) [UDP] 隧道已启动: %s -> %s", name, local, remote)
 
 	for {
-		buf := bufferPool.Get().([]byte)
+		buf := m.getBuf()
 		// 读取本地客户端的数据包，准备转发给服务端
 		n, cliAddr, err := conn.ReadFromUDP(buf)
 		if err != nil {
-			putBack(buf)
+			m.putBuf(buf)
 			if m.isClosed(err) {
 				return
 			}
@@ -266,75 +290,137 @@ func (m *Manager) runUDP(local, remote, name string) {
 			continue
 		}
 
-		key := cliAddr.AddrPort()
-		// 第一次检查(读锁)，会话是否已存在
-		mu.RLock()
-		s, ok := sessions[key]
-		mu.RUnlock()
-
-		if !ok {
-			rConn, err := net.DialUDP("udp", nil, rAddr)
-			if err != nil {
-				putBack(buf)
-				log.Printf("[WARN] (%s) 无法创建远程UDP连接: %v", name, err)
-				continue
-			}
-			_ = rConn.SetReadBuffer(2 * 1024 * 1024)
-			_ = rConn.SetWriteBuffer(2 * 1024 * 1024)
-
-			// 二次检查，防止并发创建(写锁)
-			mu.Lock()
-			if sExist, ok := sessions[key]; ok {
-				// 小概率并发冲突，使用已存在的，关闭新建的
-				_ = rConn.Close()
-				s = sExist
-			} else {
-				s = &udpSession{conn: rConn}
-				s.lastActive.Store(time.Now().Unix())
-				sessions[key] = s
-
-				// 启动反向回包协程(将服务端返回的数据转发回本地客户端)
-				m.wg.Go(func() {
-					defer func() {
-						_ = s.conn.Close()
-						mu.Lock()
-						delete(sessions, key)
-						mu.Unlock()
-					}()
-					// 复用buffer(同步串行是安全的)
-					localBuf := bufferPool.Get().([]byte)
-					defer putBack(localBuf)
-
-					for {
-						_ = s.conn.SetReadDeadline(time.Now().Add(DefaultUDPTimeout))
-						// 从远程服务端读取数据
-						rn, err := s.conn.Read(localBuf)
-						if err != nil {
-							if m.isClosed(err) {
-								return
-							}
-							if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-								log.Printf("[INFO] (%s) UDP 会话长时间无数据，自动关闭", name)
-								return
-							}
-
-							log.Printf("[WARN] (%s) 会话异常中断: %v", name, err)
-							return
-						}
-
-						s.lastActive.Store(time.Now().Unix())
-						// 将数据发给本地客户端
-						_, _ = conn.WriteToUDP(localBuf[:rn], net.UDPAddrFromAddrPort(key))
-					}
-				})
-			}
-			mu.Unlock()
+		s := m.getOrCreateSession(
+			conn,
+			sessions,
+			&mu,
+			cliAddr,
+			rAddr,
+			name,
+		)
+		if s == nil {
+			m.putBuf(buf)
+			continue
 		}
 
 		s.lastActive.Store(time.Now().Unix())
 		// 将数据发给服务端
 		_, _ = s.conn.Write(buf[:n])
-		putBack(buf)
+		m.putBuf(buf)
+	}
+}
+
+func (m *Manager) sessionsCleanup(sessions map[netip.AddrPort]*udpSession, mu *sync.RWMutex) {
+	ticker := time.NewTicker(m.cleanupInterval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-m.ctx.Done():
+			// 主动退出时，清理所有会话
+			mu.Lock()
+			for _, s := range sessions {
+				_ = s.conn.Close()
+			}
+			mu.Unlock()
+			return
+		case <-ticker.C:
+			now := time.Now().Unix()
+			mu.Lock()
+			for k, s := range sessions {
+				// 清理不活动的会话
+				if now-s.lastActive.Load() > int64(m.sessionTimeout.Seconds()) {
+					_ = s.conn.Close()
+					delete(sessions, k)
+				}
+			}
+			mu.Unlock()
+		}
+	}
+}
+
+func (m *Manager) getOrCreateSession(
+	lConn *net.UDPConn,
+	sessions map[netip.AddrPort]*udpSession,
+	mu *sync.RWMutex,
+	cliAddr *net.UDPAddr,
+	rAddr *net.UDPAddr,
+	name string,
+) *udpSession {
+	key := cliAddr.AddrPort()
+
+	mu.RLock()
+	s, ok := sessions[key]
+	mu.RUnlock()
+
+	if ok {
+		return s
+	}
+
+	rConn, err := net.DialUDP("udp", nil, rAddr)
+	if err != nil {
+		log.Printf("[WARN] (%s) 无法创建远程UDP连接: %v", name, err)
+		return nil
+	}
+	_ = rConn.SetReadBuffer(m.socketBufSize)
+	_ = rConn.SetWriteBuffer(m.socketBufSize)
+
+	// 二次检查，防止并发创建(写锁)
+	mu.Lock()
+	if sExist, ok := sessions[key]; ok {
+		// 小概率并发冲突，使用已存在的，关闭新建的
+		_ = rConn.Close()
+		s = sExist
+	} else {
+		s = &udpSession{conn: rConn}
+		s.lastActive.Store(time.Now().Unix())
+		sessions[key] = s
+	}
+
+	m.wg.Go(func() {
+		m.proxyUDPBackwards(lConn, s, sessions, mu, key, name)
+	})
+
+	return s
+}
+
+func (m *Manager) proxyUDPBackwards(
+	lConn *net.UDPConn,
+	s *udpSession,
+	sessions map[netip.AddrPort]*udpSession,
+	mu *sync.RWMutex,
+	key netip.AddrPort,
+	name string,
+) {
+	defer func() {
+		_ = s.conn.Close()
+		mu.Lock()
+		delete(sessions, key)
+		mu.Unlock()
+	}()
+	// 复用buffer(同步串行是安全的)
+	localBuf := m.getBuf()
+	defer m.putBuf(localBuf)
+
+	for {
+		_ = s.conn.SetReadDeadline(time.Now().Add(m.sessionTimeout))
+		// 从远程服务端读取数据
+		rn, err := s.conn.Read(localBuf)
+		if err != nil {
+			if m.isClosed(err) {
+				return
+			}
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				log.Printf("[INFO] (%s) UDP 会话长时间无数据，自动关闭", name)
+				return
+			}
+
+			log.Printf("[WARN] (%s) 会话异常中断: %v", name, err)
+			return
+		}
+
+		s.lastActive.Store(time.Now().Unix())
+		// 将数据发给本地客户端
+		_, _ = lConn.WriteToUDP(localBuf[:rn], net.UDPAddrFromAddrPort(key))
 	}
 }
 
@@ -368,8 +454,7 @@ func main() {
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		log.Fatalf("[ERROR] 配置文件解析错误: %v", err)
 	}
-
-	mgr := NewManager(ctx)
+	mgr := NewManager(ctx, cfg)
 	mgr.Start(cfg)
 
 	<-ctx.Done()
